b7a90b52e34bf2b699bda917ab143c3202e2b1df
Jui-Fa Tsai <juifa.tsai@gogoro.com>
2019-02-26 16:41:52 +0800

    Add to 2D

--- kriging/kriging_ordinary.py
+++ kriging/kriging_ordinary.py
@@ -1,18 +1,22 @@
 import numpy as NP
 import scipy.linalg
 import matplotlib.pyplot as PLT
+import rasterio as RAST
+from rasterio.transform import from_origin
+from shapely.geometry import Point
 from tqdm import tqdm as TQDM
 from scipy.spatial import cKDTree
 from scipy.spatial.distance import cdist
 from .variogram import variogram as VAR
-from .utility import get_neighors_brutal
+from .utility import get_neighors_brutal, create_batchrange
 
 algorithms = ['kdtree', 'brutal']
 
 class kriging_ordinary(VAR):
 
     def __init__(self, variogram=None, distance_type='euclidean', lag_range=None, lag_max=NP.inf, variogram_model='poly1', variogram_params=None, variogram_paramsbound=None, n_jobs=1, variogram_good_lowbin_fit=False, tqdm=False, debug=False, algorithm='kdtree'):
-     
+    
+        self._X = None
         self.n_features = 0
         self.params = variogram_params 
         self.params_bound = variogram_paramsbound
@@ -105,16 +109,16 @@ class kriging_ordinary(VAR):
         '''
         if X is not None:
             if self._algorithm == 'kdtree':
-                self.X = cKDTree(X, copy_data=True)
+                self._X = cKDTree(X, copy_data=True)
             else:
-                self.X = X
+                self._X = X
         if y is not None: 
             self.y = y
 
         if self._algorithm == 'kdtree': 
-            self.fit(self.X.data, self.y, to=to, transparent=transparent, show=show)
+            self.fit(self._X.data, self.y, to=to, transparent=transparent, show=show)
         else:
-            self.fit(self.X, self.y, to=to, transparent=transparent, show=show)
+            self.fit(self._X, self.y, to=to, transparent=transparent, show=show)
 
     def fit(self, X, y, to=None, transparent=True, show=False):
         '''
@@ -159,9 +163,9 @@ class kriging_ordinary(VAR):
 
         self.y = y
         if self._algorithm == 'kdtree':
-            self.X = cKDTree(X, copy_data=True)
+            self._X = cKDTree(X, copy_data=True)
         else:
-            self.X = X
+            self._X = X
 
         if len(NP.shape(X)) == 1:
             self.n_features = 1
@@ -202,6 +206,7 @@ class kriging_ordinary(VAR):
             X          : array-like, input data with same number of fearture in training data
             n_neighbor : int,        number of neighbor w.r.t input data, while distance < searching radius (5)
             radius     : float,      searching radius w.r.t input data (inf)
+            use_nugget : bool,       if use nugget to be diagonal of kriging matrix for prediction calculation (False)
             get_error  : bool,       if return error (False)
         [OUTPUT]
             1D/2D array(float)
@@ -232,13 +237,13 @@ class kriging_ordinary(VAR):
             if self.debug:
                 print('>> [INFO] Finding closest neighbors with kdtree....')
             if self.distance_type == 'cityblock':
-                neighbor_dst, neighbor_idx = self.X.query(X, k=n_neighbor, p=1 )
+                neighbor_dst, neighbor_idx = self._X.query(X, k=n_neighbor, p=1 )
             else:
-                neighbor_dst, neighbor_idx = self.X.query(X, k=n_neighbor, p=2 )
+                neighbor_dst, neighbor_idx = self._X.query(X, k=n_neighbor, p=2 )
         else:
             if self.debug:
                 print('>> [INFO] Finding closest neighbors with brutal loops....')
-            neighbor_dst, neighbor_idx = get_neighors_brutal( X, self.X, k=n_neighbor, distance=self.distance, n_jobs=self.n_jobs, tqdm=self.tqdm)
+            neighbor_dst, neighbor_idx = get_neighors_brutal( X, self._X, k=n_neighbor, distance=self.distance, n_jobs=self.n_jobs, tqdm=self.tqdm)
 
         ## Calculate prediction
         if self.debug:
@@ -270,9 +275,9 @@ class kriging_ordinary(VAR):
 
             ## Fill matrix a
             if self._algorithm == 'kdtree': 
-                D = cdist(self.X.data[ni], self.X.data[ni], metric=self.distance_type)
+                D = cdist(self._X.data[ni], self._X.data[ni], metric=self.distance_type)
             else:
-                D = cdist(self.X[ni], self.X[ni], metric=self.distance_type)
+                D = cdist(self._X[ni], self._X[ni], metric=self.distance_type)
             a[:n, :n] = self._variogram.predict(D)
             a[:, n] = 1
             a[n, :] = 1
@@ -343,4 +348,108 @@ class kriging_ordinary(VAR):
         if to is not None:
             print('>> [INFO] Saving plot to %s'% to)
             PLT.savefig(to, transparent=transparent)
+
+    def to_2D(self, xmin, xmax, ymin, ymax, cellsize=50, n_neighbor=5, radius=NP.inf, use_nugget=False, get_error=False, n_cell_per_job=1000, tqdm=False, crs='epgs:3826', to_raster=None, ):
+        """
+        [DESCRIPTION]
+            Output raster data of prediction w.r.t. given extent size
+        [INPUT]
+            xmin      : float,  minimum value of x-axis of extent (None) 
+            xmax      : float,  maximum value of x-axis of extent (None) 
+            ymin      : float,  minimum value of y-axis of extent (None)
+            ymax      : float,  maximum value of y-axis of extent (None)
+            cellsize  : flaot,  cell size of extent (50)
+            n_neighbor: int,    number of neighbors for model prediction (5)
+            radius    : float,  searching radius w.r.t input data (inf)
+            use_nugget: bool,   if use nugget to be diagonal of kriging matrix for prediction calculation (False)
+            get_error : bool,   if return error (False)
+            n_cell_per_job : int, number of cell in processing batch jobs (1000)
+            tqdm      : bool,   if show the tqdm processing bar (False)
+            crs       : string, the projection code for output raster tif ('epgs:3826')
+            to_raster : string, path to save raster tif. If None, the function return the values only (None) 
+        [OUTPUT]
+            predicted value,
+            (predicted error),
+            grid of extent
+        """
+        if self._X is None:
+            print(">> [ERROR] do fit() before calling to_2D()")
+            return
+
+        ## Create 2D extent
+        X, Y = NP.meshgrid( NP.arange(xmin, xmax+cellsize, cellsize),
+                            NP.arange(ymin, ymax+cellsize, cellsize))
+        xy = NP.concatenate(( X.flatten()[:, NP.newaxis], 
+                              Y.flatten()[:, NP.newaxis]), 
+                            axis=1)
+
+        ## Define variables
+        xbins = X.shape[1]
+        ybins = Y.shape[0]
+        z = NP.array([])
+        if get_error:
+            ze = NP.array([])
+
+        ## Create batches 
+        batches = create_batchrange( len(xy), int(len(xy)/n_cell_per_job) )
+        if self.debug: 
+            print(">> [INFO] Interpolating %d pixels (%d, %d) to %d batches:"%(len(xy), xbins, ybins, len(batches)))
+        if tqdm:
+            batches= TQDM(batches)
+
+        ## Predict values
+        for idx in batches:
+            if tqdm:
+                batches.set_description(">> ")
+            if get_error:
+                z_new, ze_new = self.predict( X          = xy[idx[0]:idx[1], :], 
+                                              n_neighbor = n_neighbor, 
+                                              radius     = radius, 
+                                              use_nugget = use_nugget, 
+                                              get_error  = True )
+                ze = NP.append(ze, ze_new)
+            else:
+                z_new = self.predict( X          = xy[idx[0]:idx[1], :], 
+                                      n_neighbor = n_neighbor, 
+                                      radius     = radius, 
+                                      use_nugget = use_nugget, 
+                                      get_error  = False )
+            z = NP.append(z, z_new)
+
+        ## Set raster left-top's coordinates
+        if to_raster is not None:
+            ### Set raster left-top's coordinates
+            transform = from_origin(xy[:,0].min(), xy[:,1].max(), cellsize, cellsize)
+
+            ### Writing raster
+            raster = RAST.open( to_raster, 
+                                'w',
+                                driver='GTiff',
+                                height=ybins,
+                                width=xbins,
+                                count=1,
+                                dtype=z.dtype,
+                                crs=crs,
+                                transform=transform )
+            raster.write(NP.flip(z.reshape(ybins,xbins), 0), 1)
+            raster.close()
+
+            if get_error:
+                raster = RAST.open( to_raster, 
+                                    'w',
+                                    driver='GTiff',
+                                    height=ybins,
+                                    width=xbins,
+                                    count=1,
+                                    dtype=ze.dtype,
+                                    crs=crs,
+                                    transform=transform )
+                raster.write(NP.flip(ze.reshape(ybins,xbins), 0), 1)
+                raster.close()
+
+        if get_error:
+            return z, ze, xy
+        else:
+            return z, xy
+
 
