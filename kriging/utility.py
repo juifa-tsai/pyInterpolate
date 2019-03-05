8c300121325cfc8678e74e96d73d57be5beba96e
Jui-Fa Tsai <juifa.tsai@gogoro.com>
2019-03-05 11:17:36 +0800

    Update brute search

--- kriging/utility.py
+++ kriging/utility.py
@@ -11,7 +11,7 @@ def create_batchrange( n_data, n_jobs ):
     idxs.append(n_data)
     return [[idxs[n], idxs[n+1]] for n in range(n_jobs)]
 
-def get_neighors_brutal(X1, X2, k, distance_type='euclidean', n_jobs=50, tqdm=False):
+def get_neighors_brutal(X1, X2, k=None, distance_type='euclidean', n_jobs=50, tqdm=False):
     '''
     Brutal method to find the k of closest X2 w.r.t X1
     X1 : interest points
